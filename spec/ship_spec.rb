b8344667b28e676ed2eb423be0884862c14aaed1
Kevin Gonzalez <kevinglz619@gmail.com>
2024-01-02 15:02:31 -0800

    add ship_spec file and first two tests

---
+++ spec/ship_spec.rb
@@ -0,0 +1,17 @@
+require './lib/ship'
+
+RSpec.describe Ship do
+
+    it "exists" do
+        cruiser = Ship.new("Cruiser", 3)
+    expect(cruiser).to be_an_instance_of(Ship)
+    end
+
+    it "has attributes" do
+        cruiser = Ship.new("Cruiser", 3)
+
+        expect(cruiser.name).to be("Cruiser")
+        expect(cruiser.lenght).to be(3)
+    end
+end
+
