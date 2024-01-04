fdcd1872bb678d1bc70c7fe9b720d9bd99a5e8f1
Kevin Gonzalez <kevinglz619@gmail.com>
2024-01-04 13:33:23 -0800

    add placement specifications to board spec file

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -46,10 +46,22 @@ RSpec.describe Board do
       
       end
 
-      it "can validate ship placement" do
+      it "can validate ship placement by length" do
         expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A3"])).to eq true
         expect(@board.valid_placement?(@submarine,["A1"])).to eq false
       end
+      it "Can validate placement by checking consecutive coordinates" do
+          
+          expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A4"])).to eq false
+          expect(@board.valid_placement?(@submarine, ["A1", "C1"])).to eq false
+          expect(@board.valid_placement?(@cruiser, ["A3", "A2", "A1"])).to eq false
+          expect(@board.valid_placement?(@submarine, ["C1", "B1"])).to eq false
+          expect(@board.valid_placement?(@submarine, ["B1", "B2"])).to eq true
+      end
+
+          
+
+        
     end
 
 
